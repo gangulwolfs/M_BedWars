90295e833346f9f76d1ebc38d3665ce16a7fe550
gauls <eungae>
2024-12-30 21:21:38 +0900

    Initial commit

---
+++ src/main/java/net/mc/gaul/m_bedwars/M_BedWars.java
@@ -0,0 +1,17 @@
+package net.mc.gaul.m_bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class M_BedWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
